e8eebf47986ce338128e5b6b693f07e1771c80d4
Roman <pv_roman@yahoo.com>
2022-11-17 16:29:32 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,64 @@
+"use strict";
+
+
+
+/* let number = 5;
+const leftBorderWidth = 1; */
+
+/* number = 10;
+console.log(number); */
+
+//Объекты:
+
+/* let instructor = {
+  name: "Viktor",
+  age: 20,
+  cityzenship: "Rus",
+  experience: true,
+}; */
+
+/* console.log(instructor); */
+/* console.log(`Имя: `, instructor.name);
+console.log(`Возраст: `, instructor.age, `лет`);
+console.log(`Гражданство: `, instructor.cityzenship); */
+
+//Массивы
+/* 
+let docs = ['passport', 'Id card', 'certificate', 'SSN'];
+console.log(docs[3], `and`, docs[2]);
+ */
+
+//alert('Hello');
+
+//const result = confirm('Are you sure?');
+//console.log(result);
+
+/* const answer = prompt("Введите Ваше имя", "");
+console.log(answer);
+console.log(typeof(answer)); */
+
+//Интерполяция
+
+/* const answers = [];
+answers[0] = prompt('Ваше имя?', '');
+answers[1] = prompt("ваша фамилия?", "");
+answers[2] = prompt("Укажите год рождения", "");
+
+console.log(answers);
+console.log(`Пользователь: ${answers[0]} ${answers[1]}`) */
+
+let incr = 10,
+    decr = 10;
+
+//incr++;
+//decr--;
+
+console.log(incr++); //постфиксная форма
+console.log(--decr); //префиксная форма
+
+console.log(incr);
+console.log(decr);
+
+console.log(7%3);
+
+
